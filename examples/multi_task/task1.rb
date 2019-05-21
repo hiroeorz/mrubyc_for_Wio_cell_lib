a8cf359983fcdee08eb64ae57f047de714461ac1
Shin Hiroe <hiroe.orz@gmail.com>
2019-05-21 18:36:21 +0900

    add multitask sample

---
+++ examples/multi_task/task1.rb
@@ -0,0 +1,9 @@
+wio = Wio.new
+arduino = Arduino.new
+
+puts "hello mruby! I am task1 !"
+
+while true
+  sleep 1
+  puts "task1"
+end
---
+++ examples/multi_task/task2.rb
@@ -0,0 +1,6 @@
+puts "hello mruby! I am task2 !"
+
+while true
+  sleep 2
+  puts "task2"
+end
