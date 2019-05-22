11d9445fdb64a1aca0caf494a459b4bd88d33ae2
Shin Hiroe <hiroe.orz@gmail.com>
2019-05-22 19:29:58 +0900

    delete unused sleep

--- examples/tcp_socket/task1.rb
+++ examples/tcp_socket/task1.rb
@@ -7,13 +7,12 @@ wio.turn_on_or_reset
 sleep 5
 wio.activate
 
-sleep 5
-sock = wio.tcp_socket_open("some.smpt.com", 25) # change your own smtp host
+sock = wio.tcp_socket_open("smtp.xxx.jp", 25) # change your own smtp host
 
 result = wio.socket_send(sock, "HELO mruby\r\n")
 puts "send #{result}"
 
-sleep 5
+sleep 1
 recv = wio.socket_receive(sock, 1024)
 puts "recv #{recv}"
 
