1db5f19abe6762565f9cc8168147d8cf2a437cec
Shin Hiroe <hiroe.orz@gmail.com>
2019-06-05 19:52:50 +0900

    MQTTの接続にブロックを使うようにした.

--- mrblib/mqtt_client.rb
+++ mrblib/mqtt_client.rb
@@ -1,22 +1,14 @@
 class MQTTClient
 
-  def self.open(host, port = 1883, id = nil)
-    puts "self.open-------------------------------------------"
-    p 0
+  def self.open(host, port, connect_id, &block)
     client = MQTTClient.new
-    success = client.connect(host, port, "hello")    
+    success = client.connect(host, port, connect_id)
 
     if success
-      client.call_block
+      block.call(client)
     else
       puts "ERROR: MQTT connect failure!"
     end
-  end
-
-  def call_block
-    puts "call_block----------------------------"
-    yield self
-    disconnect
   end
 
   def wait_loop(seconds)
